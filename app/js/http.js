95c2251d825306ff1b3b5814975f2d9b69fd22f8
zblanco <zblanco@apache.org>
2016-07-20 11:17:34 -0700

    make new request object on each request
    moved request object into the method rather than keep object-wide
    
    this allows multiple asynchronous requests

--- app/js/http.js
+++ app/js/http.js
@@ -1,23 +1,24 @@
 var client = function (responseFunc) {
 	var callback = responseFunc;
-	var req = new XMLHttpRequest();
-	req.withCredentials = true;
 	this.get = function (url) {
+		var req = new XMLHttpRequest();
+		req.withCredentials = true;
 		req.open('GET', url);
-		req.send();
-	};
-	req.onreadystatechange = function () {
-		if (req.readyState === XMLHttpRequest.DONE) {
-			if (req.status === 200) {
-				callback({ status : req.status,
-							  responseText : req.responseText
-							 });
-			} else {
-				callback({ status : req.status,
-							  responseText : "There was an error processing your request"
-							 });
+		req.setRequestHeader('Accept', "application/json")
+		req.onreadystatechange = function () {
+			if (req.readyState === XMLHttpRequest.DONE) {
+				if (req.status === 200) {
+					callback({ status : req.status,
+								  responseText : req.responseText
+								 });
+				} else {
+					callback({ status : req.status,
+								  responseText : "There was an error processing your request"
+								 });
+				}
 			}
-		}
+		};
+		req.send();
 	};
 };
 
