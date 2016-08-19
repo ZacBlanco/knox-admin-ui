5216daaeeb91b39d9bbf8923c54d2a1052f556b2
zblanco <zblanco@apache.org>
2016-08-19 13:12:46 -0700

    Add readme stuff

--- app/js/elements.js
+++ app/js/elements.js
@@ -1,21 +1,49 @@
 function createTopologyButton(name, callback) {
-	var btn = document.createElement('button');
-	btn.setAttribute('onclick', 'getTopology(\'' + name + '\')');
-	btn.innerHTML = "Click to View"
-	return btn
+  var btn = document.createElement('a');
+  btn.setAttribute('onclick', 'getTopology(\'' + name + '\')');
+  btn.className = "btn btn-default btn-block"
+  btn.setAttribute("data-toggle", "modal")
+  btn.setAttribute("data-target", "#topology-view-modal")
+  btn.innerHTML = "View"
+  return btn
 }
 
 function createSimpleElement(tagName, text) {
-	var el = document.createElement(tagName);
-	el.innerHTML = text;
-	return el;
+  var el = document.createElement(tagName);
+  if (text) {
+    el.innerHTML = text;
+  }
+  return el;
 }
 
 function createTopologyCard(topologyName, callback) {
-	var top = createSimpleElement("div", "");
-	var topName = createSimpleElement("p", topologyName)
-	top.className = "four columns card"
-	top.appendChild(topName);
-	top.appendChild(createTopologyButton(topologyName, callback));
-	return top;
+  var top = createSimpleElement("div", "");
+  top.className = "col-sm-3"
+  
+  var btn = createTopologyButton(topologyName);
+  var pan = createPanel(topologyName, 'default', btn.outerHTML);
+  
+  top.appendChild(pan)
+  return top;
+}
+
+
+function createPanel(title, type, bodyContent) {
+  var panel = createSimpleElement('div');
+  panel.className = "panel panel-" + type;
+  
+  var panelHeader = createSimpleElement('div');
+  panelHeader.className = "panel-heading";
+  
+  var panelTitle = createSimpleElement('p', title)
+  panelTitle.className = 'panel-title'
+  
+  var panelBody = createSimpleElement('div', bodyContent)
+  panelBody.className = "panel-body";
+  
+  panelHeader.appendChild(panelTitle);
+  panel.appendChild(panelHeader);
+  panel.appendChild(panelBody);
+  
+  return panel
 }
